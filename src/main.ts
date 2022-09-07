0d4a144bc186c61ab3b4d2c00e145f0648452a44
Tharlei Aleixo <tharlei_aleixo@hotmail.com>
2022-09-07 17:08:23 -0300

    feat: add vuetify lib

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import vuetify from './plugins/vuetify';
+import { loadFonts } from './plugins/webfontloader';
 import { router } from './routes';
 
-createApp(App).use(router).mount('#app');
+loadFonts();
+
+createApp(App).use(vuetify).use(router).mount('#app');
--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -2,6 +2,10 @@
 
 <template>
   <h1>+A Educação - Full Stack Web Developer</h1>
+  <v-progress-circular
+      indeterminate
+      color="primary"
+    ></v-progress-circular>
 </template>
 
 <style scoped></style>
