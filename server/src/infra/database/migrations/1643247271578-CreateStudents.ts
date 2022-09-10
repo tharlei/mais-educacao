fe7951557c64b8258c830e2dbc0887e449668bd3
Tharlei Aleixo <tharlei_aleixo@hotmail.com>
2022-09-10 17:10:00 -0300

    feat: create students table

---
+++ server/src/infra/database/migrations/1643247271578-CreateStudents.ts
@@ -0,0 +1,51 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateStudents1643247271578 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'students',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'ra',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'document',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('students');
+  }
+}
