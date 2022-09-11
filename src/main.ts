94de92900a31aea2b75f7be913e954b38f958fe5
Tharlei Aleixo <tharlei_aleixo@hotmail.com>
2022-09-11 10:34:54 -0300

    feat: add lib to mask inputs

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import App from './App.vue';
 import vuetify from './plugins/vuetify';
 import { loadFonts } from './plugins/webfontloader';
 import { router } from './routes';
+import Maska from 'maska';
 
 loadFonts();
 
-createApp(App).use(vuetify).use(router).mount('#app');
+createApp(App).use(vuetify).use(Maska).use(router).mount('#app');
