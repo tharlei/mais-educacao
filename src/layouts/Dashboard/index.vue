d62bfbe0844fb6cbc8e8ceeb3bdbf2608a6792e3
Tharlei Aleixo <tharlei_aleixo@hotmail.com>
2022-09-12 16:39:33 -0300

    feat: create dashboard layout

---
+++ src/layouts/Dashboard/index.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import Menu from '../../components/Menu/index.vue';
+import Toobar from '../../components/Toobar/index.vue';
+</script>
+
+<template>
+  <v-card>
+    <v-layout>
+      <Menu />
+      <Toobar />
+      <v-main>
+        <v-container>
+          <router-view></router-view>
+        </v-container>
+      </v-main>
+    </v-layout>
+  </v-card>
+</template>
+
+<style scoped>
+main {
+  background: #fafafa;
+  min-height: 100vh;
+}
+</style>
